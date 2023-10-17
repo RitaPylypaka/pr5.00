0c1148707b3ad39d85bfc67ded91e3e77c3e1402
RitaPylypaka <ritapylypaka@gmail.com>
2023-10-17 14:36:20 +0300

    Dasha2

---
+++ пр5.0/UnitTest5.0/UnitTest5.0.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр5.0/пр5.0.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest50
+{
+	TEST_CLASS(UnitTest50)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = h(2, 3);
+			Assert::AreEqual(t, 0.145796, 0.0000001);
+		}
+	};
+}
---
+++ пр5.2/пр5.2/пр5.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
